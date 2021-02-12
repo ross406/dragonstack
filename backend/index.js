17b4bfe437bb7217f7906e5b1f6a632e12a79cec
ross406 <roshansharma406@gmail.com>
2021-02-12 13:54:28 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const Dragon = require("./dragon.js");
+
+const fooey = new Dragon({
+    birthdate: new Date(),
+    nickname: "fooey"
+});
+const baloo = new Dragon({
+    birthdate: new Date(),
+    nickname: "baloo",
+    traits:[
+        {traitType:"backgroundColor",traitValue:"green"}
+    ]
+});
+
+const mimar = new Dragon();
+
+setTimeout(()=> {
+    const gooby = new Dragon();
+
+    console.log("gooby",gooby) 
+},2000)
+
+console.log("fooey",fooey)
+console.log("baloo",baloo) 
+console.log("mimar",mimar)
